27d7cf7a9f24c800c518f01c7575efbd9cd2eb20
Glitch (tech-mod) <none>
2021-03-07 11:56:30 +0000

    🐴💒 Checkpoint ./commands/fun/rank.py:27563752/3527

--- commands/fun/rank.py
+++ commands/fun/rank.py
@@ -1,33 +1,65 @@
 @client.event
 async def on_message(message):
-  with open('users.json', 'r') as f:
-    users = json.load(f)
+    if not message.author.bot:
+        print('function load')
+        with open('level.json','r') as f:
+            users = json.load(f)
+            print('file load')
+        await update_data(users, message.author,message.guild)
+        await add_experience(users, message.author, 4, message.guild)
+        await level_up(users, message.author,message.channel, message.guild)
 
-    await update_data(users, member)
-
-
-
-  with open("users.json", 'w') as f:
-    json.dump(users, f)
-@client.event
-async def on_message(message):
-  author = message.author
-
-  with open('users.json', 'r') as f:
-    users = json.load(f)
-
-    await update_data(users, author)
-    await add_experience(users, author, 5)
-    await level_up(users, author, message.channel)
-  with open("users.json", 'w') as f:
-    json.dump(users, f)
+        with open('level.json','w') as f:
+            json.dump(users, f)
+    await client.process_commands(message)
 
 
-  if message.content.startswith("&stats"):
-    global Pxp
-    global KA
-    Pxp = 0
-    KA = 0
-    await message.channel.send("XP: " + str(Pxp) + ". Enemy Tanks Killed: " + str(KA) + ".")
-  if message.content.startswith('&start'):
-    await message.channel.send('Choose vehicle: VT-4, M1 Abrams, T-18 Armata. 1, 2, 3.')
+
+
+async def update_data(users, user,server):
+    if not str(server.id) in users:
+        users[str(server.id)] = {}
+        if not str(user.id) in users[str(server.id)]:
+            users[str(server.id)][str(user.id)] = {}
+            users[str(server.id)][str(user.id)]['experience'] = 0
+            users[str(server.id)][str(user.id)]['level'] = 1
+    elif not str(user.id) in users[str(server.id)]:
+            users[str(server.id)][str(user.id)] = {}
+            users[str(server.id)][str(user.id)]['experience'] = 0
+            users[str(server.id)][str(user.id)]['level'] = 1
+
+async def add_experience(users, user, exp, server):
+  users[str(user.guild.id)][str(user.id)]['experience'] += exp
+
+async def level_up(users, user, channel, server):
+  experience = users[str(user.guild.id)][str(user.id)]['experience']
+  lvl_start = users[str(user.guild.id)][str(user.id)]['level']
+  lvl_end = int(experience ** (1/4))
+  
+  if lvl_start < lvl_end:
+      await channel.send('{} has leveled up to Level {}'.format(user.mention, lvl_end))
+      users[str(user.guild.id)][str(user.id)]['level'] = lvl_end
+
+
+@client.command(aliases = ['rank','lv1'])
+async def level(ctx,member: discord.Member = None):
+
+    if not member:
+        user = ctx.message.author
+        with open('level.json','r') as f:
+        users = json.load(f)
+        lvl = users[str(ctx.guild.id)][str(user.id)]['level']
+        exp = users[str(ctx.guild.id)][str(user.id)]['experience']
+
+        embed = discord.Embed(title = 'Level {}'.format(lvl), description = f"{exp} XP " ,color = discord.Color.green())
+        embed.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
+        await ctx.send(embed = embed)
+    else:
+      with open('level.json','r') as f:
+          users = json.load(f)
+      lvl = users[str(ctx.guild.id)][str(member.id)]['level']
+      exp = users[str(ctx.guild.id)][str(member.id)]['experience']
+      embed = discord.Embed(title = 'Level {}'.format(lvl), description = f"{exp} XP" ,color = discord.Color.green())
+      embed.set_author(name = member, icon_url = member.avatar_url)
+
+      await ctx.send(embed = embed)
