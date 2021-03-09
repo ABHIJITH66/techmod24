d3fbac1f8e3efca5039cde3c09e188e5af47ed3c
Glitch (tech-mod) <none>
2021-03-09 10:57:55 +0000

    🐋👵 Checkpoint ./server.js:27563752/1754 ./commands/moderation/SETWELCOMECHANNEL.js:27563752/901

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { Prefix, Token, Color } = require("./config.js");
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.db = require("quick.db");
+const moment = require("moment")
+const db = require("quick.db")
 
 
 client.on("ready", async () => {
@@ -18,7 +20,45 @@ client.on("ready", async () => {
 
 //hhhhiii
 
-
+client.on("guildMemberAdd", async member => {
+  if (member.user.bot) return;
+  let user = member.user
+  let channelID = db.get(`joinChannel_${member.guild.id}`)
+ if (channelID === null) return;
+ let channel = member.guild.channels.cache.get(channelID)
+ if (!channel) return;
+  let joinMsg = db.get(`joinmsg_${member.guild.id}`)
+  if (!joinMsg) return;
+  let send = joinMsg
+ .split("{member-mention}").join("<@" + user.id + ">")
+ .split("{member-tag}").join(user.tag)
+ .split("{member-username}").join(user.username)
+ .split("{member-id}").join(user.id)
+ .split("{member-created:duration}").join(moment(user.createdTimestamp).fromNow())
+ .split("{member-created:date}").join(moment(user.createdTimestamp).format("YYYY/MM/DD"))
+ .split("{server-name}").join(member.guild.name)
+ .split("{server-memberCount}").join(member.guild.members.cache.size)
+ channel.send(send)
+})
+client.on("guildMemberRemove", async member => {
+ if (member.user.bot) return;
+ let user = member.user
+ let channelID = db.get(`leaveChannel_${member.guild.id}`)
+ if (channelID === null) return;
+ let channel = member.guild.channels.cache.get(channelID)
+ if (!channel) return;
+ let leaveMsg = db.get(`leavemsg_${member.guild.id}`)
+ if (leaveMsg === null) return;
+ let send = leaveMsg
+ .split("{member-tag}").join(user.tag)
+ .split("{member-username}").join(user.username)
+ .split("{member-id}").join(user.id)
+ .split("{member-created:duration}").join(moment(user.createdTimestamp).fromNow())
+ .split("{member-created:date}").join(moment(user.createdTimestamp).format("YYYY/MM/DD"))
+ .split("{server-name}").join(member.guild.name)
+ .split("{server-memberCount}").join(member.guild.members.cache.size)
+ channel.send(send)
+})
  
 
 //hello
