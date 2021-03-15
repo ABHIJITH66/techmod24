4a23ee2f19cd48096d0fef1ab3b6c0b51e8da8f5
Glitch (tech-mod) <none>
2021-03-15 16:15:40 +0000

    ✈️🌋 Checkpoint ./config.js:27563752/118

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.Prefix = `=`;//your prefix for bot
-exports.Token = `ODAwMjQ1ODMxNjY0MDA5MjI3.YAPVAg.p-cZghd4TMma93Sk87su8oFnK0g`;//your token 
+exports.Token = `ODAwMjQ1ODMxNjY0MDA5MjI3.YAPVAg.dr-eDUicpCvjDpSUd0SOA8ftFS4`;//your token 
 exports.Color = `RED`;//color   of embed
