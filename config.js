fd8001bebe3f612c5dbdfe3df9de219745e9c4c7
ABHIJITH66 <82161613+ABHIJITH66@users.noreply.github.com>
2021-04-24 15:09:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.Prefix = `=`;//your prefix for bot
-exports.Token = `ODAwMjQ1ODMxNjY0MDA5MjI3.YAPVAg.dr-eDUicpCvjDpSUd0SOA8ftFS4`;//your token 
+exports.Token = `ODAwMjQ1ODMxNjY0MDA5MjI3.YAPVAg._w_QJrtcf_Coj1euRn1ZR_R9dHQ`;//your token 
 exports.Color = `RED`;//color   of embed
